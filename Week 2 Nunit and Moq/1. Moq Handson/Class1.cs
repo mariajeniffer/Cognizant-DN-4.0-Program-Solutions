bc88f595d94d720966dcfee26e4618403efcc70c
mariajeniffer <mariaajeniffer72@gmail.com>
2025-06-29 19:54:44 +0530

    Week 2 NUnit and Moq

---
+++ Week 2 Nunit and Moq/1. Moq Handson/Class1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender>? _mockMailSender;
+        private CustomerCommLib.CustomerComm? _customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(sender => sender.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [TestCase]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            // Act
+            bool result = _customerComm!.SendMailToCustomer();
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
