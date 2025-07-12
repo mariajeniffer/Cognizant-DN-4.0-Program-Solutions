c529be233b91e616241c8be5d53b9767d0fc39db
mariajeniffer <mariaajeniffer72@gmail.com>
2025-07-12 21:53:21 +0530

    Add files via upload
    Week 4 WebAPI_HandsOn

---
+++ EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Authorize(Roles = "Admin,POC")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "You are authorized to view this!" });
+        }
+    }
+}
+
+
